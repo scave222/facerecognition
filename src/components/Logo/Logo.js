31a7be77d81ba4bee14bd07f5889ca3c7a2abf59
caleb okogbe <okogbecaleb@gmail.com>
2019-12-01 18:02:36 +0100

    dc

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css'
+import calto from './calto.jpg'
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-3" options={{ max : 55 }} style={{ height: 113, width: 150 }} >
+                <div className="Tilt-inner"> <img alt='logo' src={calto}/> </div>
+            </Tilt>
+
+        </div>
+    );
+}
+
+export default Logo;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = () => {
+    return (
+        <nav >
+            <p className='f3 link dim underline pa3 pointer'>Sign Out</p>
+            
+        </nav>
+    );
+}
+
+export default Navigation;
